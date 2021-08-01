932e26fa49b69eac4c47765c6c343a5875e40707
Michael Sutton <hicallmesutton@gmail.com>
2021-07-31 23:21:40 -0500

    i think the issue is the ENV is not being loaded

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://draword.herokuapp.com'
+  config.action_cable.url = 'wss://draword.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://miloscomplex.github.io', /http:\/\/miloscomplex.github.io.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
